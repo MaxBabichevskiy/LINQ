c3e9bdefbc53bfbae4df34c6caa429d50d7052da
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2023-04-20 14:16:29 +0300

    Add files via upload

---
+++ LINQ/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace LINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(188, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(313, 258);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(53, 21);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(53, 53);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextBox.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(12, 93);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(12, 122);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(75, 23);
+            this.editButton.TabIndex = 4;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(12, 160);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Age";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 330);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LINQ/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace LINQ
+{
+    public partial class Form1 : Form
+    {
+        private DataContext db;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Подключение к БД
+            string connectionString = "Data Source=KOMPUTER\\;Initial Catalog=adonetdb;Integrated Security=True";
+            db = new DataContext(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Получение всех пользователей
+            var users = db.GetTable<User>().ToList();
+
+            // Отображение пользователей в DataGridView
+            dataGridView1.DataSource = users;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            // Создание нового пользователя
+            User user = new User { Name = nameTextBox.Text, Age = int.Parse(ageTextBox.Text) };
+
+            // Добавление пользователя в БД
+            db.GetTable<User>().InsertOnSubmit(user);
+            db.SubmitChanges();
+
+            // Обновление DataGridView
+            dataGridView1.DataSource = db.GetTable<User>().ToList();
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            // Получение выбранного пользователя из DataGridView
+            User selectedUser = (User)dataGridView1.SelectedRows[0].DataBoundItem;
+
+            // Обновление имени и возраста пользователя
+            selectedUser.Name = nameTextBox.Text;
+            selectedUser.Age = int.Parse(ageTextBox.Text);
+
+            // Обновление пользователя в БД
+            db.SubmitChanges();
+
+            // Обновление DataGridView
+            dataGridView1.DataSource = db.GetTable<User>().ToList();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            // Получение выбранного пользователя из DataGridView
+            User selectedUser = (User)dataGridView1.SelectedRows[0].DataBoundItem;
+
+            // Удаление пользователя из БД
+            db.GetTable<User>().DeleteOnSubmit(selectedUser);
+            db.SubmitChanges();
+
+            // Обновление DataGridView
+            dataGridView1.DataSource = db.GetTable<User>().ToList();
+        }
+    }
+
+    [Table(Name = "User")]
+    public class User
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id { get; set; }
+
+        [Column]
+        public string Name { get; set; }
+
+        [Column]
+        public int Age { get; set; }
+    }
+}
